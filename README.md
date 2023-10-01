c226399c213754bd119fe045802610e9304a3f24
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-01 19:26:39 +0300

    correct misspelled property

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ This dashboard is created from scratch with React.
 ### Theme Setup
 The theme colors were choosed based on #666666, #141b2d, #4cceac, #db4f4a, #6870fa. Using the Tailwind Shades vscode extension,
 I was able to generate shades of theese main colors for the dark mode (by selecting the color and press Cmd+K and Cmd+G). The colors of the light mode were basically the same colors but sorted descendent with few exceptions.
+
--- src/theme.js
+++ src/theme.js
@@ -138,9 +138,9 @@ export const themeSettings = (mode) => {
                         main: colors.greenAccent[500],
                     },
                     neutral: {
-                        dark: colors.grey[700],
-                        main: colors.grey[500],
-                        light: colors.grey[100],
+                        dark: colors.gray[700],
+                        main: colors.gray[500],
+                        light: colors.gray[100],
                     },
                     background: {
                         default: colors.primary[500],
@@ -153,9 +153,9 @@ export const themeSettings = (mode) => {
                         main: colors.greenAccent[500],
                     },
                     neutral: {
-                        dark: colors.grey[700],
-                        main: colors.grey[500],
-                        light: colors.grey[100],
+                        dark: colors.gray[700],
+                        main: colors.gray[500],
+                        light: colors.gray[100],
                     },
                     background: {
                         default: '#fcfcfc',
