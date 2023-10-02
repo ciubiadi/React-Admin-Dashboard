0ffee4476ea6ff1900896aa635486be7ea859941
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-02 20:26:53 +0300

    Build Header component and use it in Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 const Dashboard = () => {
-    return (<div>Dashboard</div>)
+    return (<Box>
+        <Header title="DASHBOARD" subtitle="Welcome to yourdashboard" />
+    </Box>)
 }
 
 export default Dashboard;
