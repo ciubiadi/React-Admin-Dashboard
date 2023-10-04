a4e8af1f3c6f899e6d456288a04c98cd7f58a8b3
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-04 23:21:30 +0300

    add events State and configure FullCalendar header

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 import Header from '../../components/Header'
 import { Box } from '@mui/material'
 
 const Calendar = () => {
-  
-    return (
-      <Box m="20px">
-        <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
-        <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-        />
-      </Box>
-    )
+
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  return (
+    <Box m="20px">
+      <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+      <FullCalendar
+        height="75vh"
+        plugins={[ 
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          listPlugin 
+        ]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth, timeGridWeek, timeGridDay, listMonth',
+        }}
+        initialView="dayGridMonth"
+      />
+    </Box>
+  )
   
 }
 export default Calendar;
