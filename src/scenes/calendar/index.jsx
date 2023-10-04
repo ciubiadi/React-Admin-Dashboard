fffbda27787a77835028e73c0b20c135644c6491
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-04 23:10:45 +0300

    put the calendar into a Box

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import Header from '../../components/Header'
+import { Box } from '@mui/material'
 
 const Calendar = () => {
   
     return (
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
+      <Box m="20px">
+        <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
+        <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+        />
+      </Box>
     )
   
 }
